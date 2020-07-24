d3c1aced367db8e3fbfd1ef7da5548de8f1599d5
Yifang Yuan <yyuan@nbai.io>
2020-07-24 11:25:12 -0400

    first commit

--- src/components/BarChart.vue
+++ src/components/BarChart.vue
@@ -10,7 +10,7 @@
                 default: null
             },
             options:{
-                type:Object,
+                type:Object
                 default:null
             }
         },
